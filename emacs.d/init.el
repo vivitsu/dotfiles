833b4812bb1aed404a400ae510c294707c6005cf
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-04-15 22:51:14 -0700

    Add latest versions of .emacs and .bashrc

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,12 +1,39 @@
+(setq user-full-name "Vivitsu Maharaja")
+(setq user-mail-address "vivitsu.maharaja@gmail.com")
+
+;; set a default font
+(when (member "Monaco" (font-family-list))
+  (set-face-attribute 'default nil :font "Monaco"))
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/base16")
-(load-theme 'base16-default t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'zenburn t)
 
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives
+	     '("marmalade" . "https://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa") t) ; Org-mode's repository
 (package-initialize)
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+(setq backup-by-copying t    ; Don't clobber symlinks
+      backup-directory-alist
+      '(("." . "~/.saves"))  ; Don't litter my fs tree
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)     ; Use versioned backups
+
+(setq inhibit-splash-screen t
+      initial-scratch-message nil)
+
+(setq tab-width 4
+      indent-tabs-mode nil)
+
+;; Always highlight parentheses
+(show-paren-mode t)
+
