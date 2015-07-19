bf1b82793335b5196ea280fe9f1639eb7142e72d
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-07-19 11:46:49 -0700

    Change .bashrc, .emacs, .gitconfig
    .bashrc:
    
    - Add cabal bin to $PATH
    - Change $EDITOR setting and alias to use `emacsclient`
    
    .emacs:
    
    - Don't start server from GUI emacs
    - Load different themes on GUI/Terminal
    - Don't make backups of files
    - Don't show tool bar and scroll bar in GUI
    - Set emacs $PATH to be the same as bash $PATH
    
    .gitconfig:
    
    - Change `emacsclient` config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,32 +1,26 @@
 (setq user-full-name "Vivitsu Maharaja")
 (setq user-mail-address "vivitsu.maharaja@gmail.com")
 
-(server-start)
-
 ;; set a default font
 (when (member "Monaco" (font-family-list))
   (set-face-attribute 'default nil :font "Monaco"))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/base16")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'zenburn t)
+(if (display-graphic-p)
+    (load-theme 'smyx t)
+  (load-theme 'wombat t))
 
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
-	     '("marmalade" . "https://marmalade-repo.org/packages/") t)
+	     '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
 	     '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
 (package-initialize)
 
-(setq backup-by-copying t    ; Don't clobber symlinks
-      backup-directory-alist
-      '(("." . "~/.saves"))  ; Don't litter my fs tree
-      delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      version-control t)     ; Use versioned backups
+(setq make-backup-files nil)
 
 (setq inhibit-splash-screen t
       inhibit-startup-echo-area-message t
@@ -36,7 +30,10 @@
       indent-tabs-mode nil
       column-number-mode t)
 
-(tool-bar-mode -1)
+(if (display-graphic-p)
+    (progn
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)))
 
 ;; Swap option and command keys on Mac OS X
 (cond
@@ -46,24 +43,25 @@
  )
 )
 
+;; Set $PATH to bash PATH on Mac OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 ;; Always highlight parentheses
 (show-paren-mode t)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; Add ghc-mod
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-;; Add python-mode
-(add-to-list 'load-path "~/.emacs.d/plugins/python-mode.el-6.2.0")
-;;(setq py-install-directory "~/.emacs.d/plugins/python-mode.el-6.2.0")
+; Add python-mode
+(add-to-list 'load-path "~/.emacs.d/plugins/python-mode.el-6.2.0") 
+;(setq py-install-directory "~/.emacs.d/plugins/python-mode.el-6.2.0")
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (require 'python-mode)
-
-
-
-
-
-
-
